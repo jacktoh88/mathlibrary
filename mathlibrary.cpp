65734e6ba07e1256c8533c1919fd00e06cdc1d8d
jacktoh88 <tohc88@outlook.com>
2019-09-07 00:02:06 +0800

    "再加移项矩阵函数“

--- mathlibrary.cpp
+++ mathlibrary.cpp
@@ -27,4 +27,63 @@ void transpose1x3(double (&in)[1][3], double (&out)[3][1]){
     }
 }
 
+//移项矩阵
+void transpose2x1(double (&in)[2][1], double (&out)[1][2]){
+
+    for (int i=0; i<2; i++){ // 行
+    
+        for (int j=0; j<1; j++){ // 列
+        
+            out[j][i] = in[i][j];
+        }
+    }
+}
+
+//移项矩阵
+void transpose2x2(double (&in)[2][2], double (&out)[2][2]){
+
+    for (int i=0; i<2; i++){ // 行
+    
+        for (int j=0; j<2; j++){ // 列
+        
+            out[j][i] = in[i][j];
+        }
+    }
+}
+
+//移项矩阵
+void transpose2x3(double (&in)[2][3], double (&out)[3][2]){
+
+    for (int i=0; i<2; i++){ // 行
+    
+        for (int j=0; j<3; j++){ // 列
+        
+            out[j][i] = in[i][j];
+        }
+    }
+}
+
+//移项矩阵
+void transpose3x2(double (&in)[3][2], double (&out)[2][3]){
+
+    for (int i=0; i<3; i++){ // 行
+    
+        for (int j=0; j<2; j++){ // 列
+        
+            out[j][i] = in[i][j];
+        }
+    }
+}
+
+//移项矩阵
+void transpose3x3(double (&in)[3][3], double (&out)[3][3]){
+
+    for (int i=0; i<3; i++){ // 行
+    
+        for (int j=0; j<3; j++){ // 列
+        
+            out[j][i] = in[i][j];
+        }
+    }
+}
 
